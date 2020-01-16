fba82c0c83dc3ec6f605be73ab5f2abb6480a127
Adam La Fleur <alafleur938@gmail.com>
2020-01-16 14:55:56 -0800

    Updated

--- frupal/__main__.py
+++ frupal/__main__.py
@@ -1,12 +1,17 @@
-from .config import Config
-from .player import Player
+from config import Config
+from player import Player
+from drawer import Drawer
 
 
 if __name__ == "__main__":
-    config = Config.load_config()
-    # Make the player
-    player = Player(energy=config.start_energy, money=config.start_money)
-    playing = True
-    while playing:
+    #config = Config.load_config()
+    #Make the player
+	#player = Player(energy=config.start_energy, money=config.start_money)
+	playing = True
+	drawer = Drawer()
+	drawer.titlescreen()
+	drawer.menuscreen()
+	drawer.printmap()
+	while playing:
         # TODO: do loop
-        playing = False
+		playing = False
--- frupal/drawer.py
+++ frupal/drawer.py
@@ -1,5 +1,41 @@
-
+import crayons
+import os
+import time
 
 class Drawer:
-    def __init__(self):
-        pass
+	a = None
+	width = os.get_terminal_size().columns
+	height = os.get_terminal_size().lines
+	middle = height // 2
+
+	def __init__(self):
+        #x = os.get_terminal_size().columns
+		#y = os.get_terminal_size().lines
+		x = 20
+		y = 20
+		self.a = ['T'] * y
+		for i in range(y):
+			self.a[i] = ['T'] * x
+		
+	def titlescreen(self):
+		for i in range(self.middle):
+			print()
+		print(crayons.green("The Game of Frugal!".center(self.width)))
+		for i in range(self.middle):
+			print()
+		time.sleep(2)
+	
+	def menuscreen(self):
+		for i in range(self.middle - 2):
+			print()
+		print(crayons.green("The Game of Frugal!".center(self.width)))
+		print("Start Game!".center(self.width))
+		for i in range(self.middle - 2):
+			print()
+		time.sleep(2)
+
+	def printmap(self):
+		for j in range(len(self.a)):
+			for k in range(len(self.a[j])):
+				print(crayons.green(self.a[j][k]), end = ' ')
+			print()

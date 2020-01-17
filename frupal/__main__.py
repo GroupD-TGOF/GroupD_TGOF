2524862991b91f64fb0c19b06a8475727c744cc2
Adam La Fleur <alafleur938@gmail.com>
2020-01-16 23:36:34 -0800

    Update __main__.py
    Code causes recursive loop, RecursionError: maximum recursion depth exceeded while calling a Python object. Commented out config in __main__ until fix

--- frupal/__main__.py
+++ frupal/__main__.py
@@ -4,14 +4,15 @@ from drawer import Drawer
 
 
 if __name__ == "__main__":
-    #config = Config.load_config()
-    #Make the player
-	#player = Player(energy=config.start_energy, money=config.start_money)
-	playing = True
-	drawer = Drawer()
-	drawer.titlescreen()
-	drawer.menuscreen()
-	drawer.printmap()
-	while playing:
-        # TODO: do loop
-		playing = False
+        #Code causes recursive loop, RecursionError: maximum recursion depth exceeded while calling a Python object
+        #config = Config.load_config()
+        #Make the player
+        #player = Player(energy=config.start_energy, money=config.start_money)
+        playing = True
+        drawer = Drawer()
+        drawer.titlescreen()
+        drawer.menuscreen()
+        drawer.printmap()
+        while playing:
+                # TODO: do loop
+                playing = False

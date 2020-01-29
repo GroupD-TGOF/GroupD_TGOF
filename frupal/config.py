a29ee4c828be72b8a1e7425e841130d8eb0d5448
matthewcole805 <51109949+matthewcole805@users.noreply.github.com>
2020-01-28 17:52:21 -0800

    Update config.py

--- frupal/config.py
+++ frupal/config.py
@@ -1,11 +1,26 @@
+from enum import Enum
+class Size(Enum):
+        Small = 1
+        Medium = 2
+        Large = 3
+
+class Style(Enum):
+        Park = 1
+        Forest = 2
+        Rocky_Forest = 3
+        Rain_Forest = 4
+        Marsh = 5
+        Rocky_Swamp = 6
+        Quarry = 7
+
 class Config:
     static = None
 
     def __init__(self):
         self.map = {
-            'total': 0
-            'height': 0
-            'width': 0
+            'total': 0,
+            'height': 0,
+            'width': 0,
             'trees': 0,
             'boulders': 0,
             'water': 0,
@@ -16,13 +31,14 @@ class Config:
             'energy': 0,
             'money': 10
         }
-        print("Welcome to Frupal!")
 
+    def map_size(self):
         print("1. Small (10 x 10")
         print("2. Medium (20 x 20")
         print("3. Large (20 x 40")
-        print("input size of map: ")
-        inputSize = raw_input()
+        print("input size of map(1-3): ")
+        inputSize = int(raw_input())
+
 
         for case in switch(inputSize):
             if case(1):
@@ -39,6 +55,7 @@ class Config:
                 break
         self.map["total"] = self.map["height"] * self.map["width"]
 
+    def map_difficulty(self):
         print("1. Park")
         print("2. Forest")
         print("3. Rocky Forest")
@@ -47,9 +64,7 @@ class Config:
         print("6. Rocky Swamp")
         print("7. Quarry")
         print("Please select what kind of map you would like (ranked by difficult (1-7): ")
-        difficulty = raw_input()
-
-        print()
+        difficulty: int(raw_input())
 
         for case in switch(difficulty):
             if case(1):
@@ -99,6 +114,9 @@ class Config:
                 self.player['energy'] = self.map['total'] * .8
                 break
             if case():  # default, could also just omit condition or 'if True'
-                print
-                "something else!"
+                print("something else!")
 
+    def printSettings(self):
+        print("Creating " + size(inputSize).name + " " + style(difficulty).name + "...")
+        print(self.map['trees'] + " trees, " + self.map['boulder'] + " boulders, " + self.map['water'] + " sqrft of water, and " + self.map['mud'] + " sqrft of mud.")
+        

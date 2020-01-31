49f5f800c156666bf2ea08132da402b68ddf746d
matthewcole805 <51109949+matthewcole805@users.noreply.github.com>
2020-01-31 07:00:38 -0800

    Changed from Choices to elif statements Added get function Cleaned up print statements

--- frupal/config.py
+++ frupal/config.py
@@ -1,25 +1,18 @@
-from enum import Enum
 
 
 class Config:
     static = None
 
-    class Size(Enum):                                                   # Creates enum classes for print_settings
-        Small = 1
-        Medium = 2
-        Large = 3
-
-    class Style(Enum):
-        Park = 1
-        Forest = 2
-        Rocky_Forest = 3
-        Rain_Forest = 4
-        Marsh = 5
-        Rocky_Swamp = 6
-        Quarry = 7
-
     input_size: 0                                                       # Initializes user inputs
     input_style: 0
+
+    @staticmethod
+    def get_map(k):
+        return config.map[k]
+
+    @staticmethod
+    def get_player(k):
+        return config.player[k]
 
     def __init__(self):
         self.map = {                                                    # Creates Map Library
@@ -33,84 +26,119 @@ class Config:
         }
         self.player = {                                                  # Creates Player Library
             'energy': 0,                                                 # Initializes player stats
-            'money': 10
+            'money': 0
         }
 
+        self.map_size()
+        self.map_style()
+        self.player_stats()
+
     def map_size(self):
-        print("1. Small (10 x 10")                                         # Prints size options in ascending order
-        print("2. Medium (20 x 20")
-        print("3. Large (20 x 40")
-        self.input_size = int(input("input size of map(1-3): "))           # User inputs size choice
-        while self.input_size < 1 or self.input_size > 3:
-            print("Must be 1, 2, or 3")                                    # Ensures proper input
-            self.input_size = int(input("input size of map(1-3): "))
-            choices = {1: (10, 10),                                        # Library of corresponding Heights and widths
-                       2: (20, 20),
-                       3: (20, 40)}
-            (self.map["height"],                                           # Sets map size based on input choice
-             self.map["width"]) = choices.get(self.map["input_size"], ('default1', 'default2'))
-        self.map["total_tiles"] = self.map["height"] * self.map["width"]   # Calculates total_tiles number of tiles
+        print("Frupal Settings:")
+        print("Set your game map size:")
+        print("1. Small (10 x 10)")                                         # Prints size options in ascending order
+        print("2. Medium (20 x 20)")
+        print("3. Large (20 x 40)")           # User inputs size choice
+        while self.map['height'] == 0:                               # Ensures proper input
+            self.input_size = int(input("Enter selection (1-3): "))
+            if self.input_size == 1:
+                self.map["height"] = 10
+                self.map["width"] = 10
+            elif self.input_size == 2:
+                self.map["height"] = 20
+                self.map["width"] = 20
+            elif self.input_size == 3:
+                self.map["height"] = 20
+                self.map["width"] = 40
+            else:
+                print("Must be 1, 2, or 3")
+        self.map['total'] = self.map['height'] * self.map['width']
 
     def map_style(self):
+        print('Select your game style (Ranked by difficulty)')
         print('1. Park')                                             # Prints map style options ranked by difficulty
         print('2. Forest')
         print('3. Rocky Forest')
         print('4. Rain Forest')
         print('5. Marsh')
         print('6. Rocky Swamp')
-        print('7. Quarry')                                           # User inputs difficulty choice
-        self.input_style = int(input('Please select what kind of map you would like (ranked by difficult (1-7): '))
-        while self.input_style < 1 or self.input_style > 7:
-            print("Must be 1-7")                                     # Ensures proper input
-            self.input_style = int(input('Please select what kind of map you would like (ranked by difficult (1-7): '))
-        choices = {1: (int(self.map['total_tiles_tiles'] / 5),       # Park: trees 20%, boulders 5%, water 5%, mud 0%
-                       int(self.map['total_tiles'] / 20),            # energy 80%
-                       int(self.map['total_tiles'] / 20), 0,
-                       int(self.map['total_tiles'] * .8)),
-                   2: (int(self.map['total_tiles'] / 2),             # Forest: trees 50%, boulders 5%, water 5%, mud 0%
-                       int(self.map['total_tiles'] / 20),            # energy 50%
-                       int(self.map['total_tiles'] / 20), 0,
-                       int(self.map['total_tiles'] / 2)),
-                   3: (int(self.map['total_tiles'] / 2),        # RockyForest: trees 50%, boulders 20%, water 5%, mud 0%
-                       int(self.map['total_tiles'] / 5),        # energy 50%
-                       int(self.map['total_tiles'] / 20), 0,
-                       int(self.map['total_tiles'] / 2)),
-                   4: (int(self.map['total_tiles'] / 2),        # RainForest: trees 50%, boulders 5%, water 10%, mud 10%
-                       int(self.map['total_tiles'] / 20),       # energy 50%
-                       int(self.map['total_tiles'] / 10),
-                       int(self.map['total_tiles'] / 10),
-                       int(self.map['total_tiles'] / 2)),
-                   5: (int(self.map['total_tiles'] / 5),             # Marsh: trees 20%, boulders 5%, water 20%, mud 50%
-                       int(self.map['total_tiles'] / 20),            # energy 80%
-                       int(self.map['total_tiles'] / 5),
-                       int(self.map['total_tiles'] / 2),
-                       int(self.map['total_tiles'] * .8)),
-                   6: (int(self.map['total_tiles'] / 10),      # RockySwamp: trees 10%, boulders 20%, water 20%, mud 50%
-                       int(self.map['total_tiles'] / 5),       # energy 80%
-                       int(self.map['total_tiles'] / 5),
-                       int(self.map['total_tiles'] / 2),
-                       int(self.map['total_tiles'] * .8)),
-                   7: (int(self.map['total_tiles'] / 20),            # Quarry: trees 5%, boulders 33%, water 10%, mud 5%
-                       int(self.map['total_tiles'] / 3),             # energy 80%
-                       int(self.map['total_tiles'] / 10),
-                       int(self.map['total_tiles'] / 20),
-                       int(self.map['total_tiles'] * .8))
-                   }
-        (self.map['trees'],                                          # Selects choice based on input
-         self.map['boulder'],                                        # Assigns tile quantities and energy level
-         self.map['water'],
-         self.map['mud'],
-         self.player['energy']) = choices.get(self.input_style,
-                                              ('default1', 'default2', 'default3', 'default4', 'default5'))
+        print('7. Quarry')
+        while self.map['trees'] == 0:                                    # User inputs difficulty choice
+            self.input_style = int(input('Enter Selection (1-7): '))
+            if self.input_style == 1:
+                self.map['trees'] = int(self.map['total'] / 5)
+                self.map['boulder'] = int(self.map['total'] / 20)
+                self.map['water'] = int(self.map['total'] / 20)
+            elif self.input_style == 2:
+                self.map['trees'] = int(self.map['total'] / 2)
+                self.map['boulder'] = int(self.map['total'] / 20)
+                self.map['water'] = int(self.map['total'] / 20)
+            elif self.input_style == 3:
+                self.map['trees'] = int(self.map['total'] / 2)
+                self.map['boulder'] = int(self.map['total'] / 5)
+                self.map['water'] = int(self.map['total'] / 20)
+            elif self.input_style == 4:
+                self.map['trees'] = int(self.map['total'] / 2)
+                self.map['boulder'] = int(self.map['total'] / 20)
+                self.map['water'] = int(self.map['total'] / 10)
+                self.map['mud'] = int(self.map['total'] / 10)
+            elif self.input_style == 5:
+                self.map['trees'] = int(self.map['total'] / 5)
+                self.map['boulder'] = int(self.map['total'] / 20)
+                self.map['water'] = int(self.map['total'] / 6)
+                self.map['mud'] = int(self.map['total'] / 2)
+            elif self.input_style == 6:
+                self.map['trees'] = int(self.map['total'] / 10)
+                self.map['boulder'] = int(self.map['total'] / 6)
+                self.map['water'] = int(self.map['total'] / 6)
+                self.map['mud'] = int(self.map['total'] / 2)
+            elif self.input_style == 7:
+                self.map['trees'] = int(self.map['total'] / 20)
+                self.map['boulder'] = int(self.map['total'] / 6)
+                self.map['water'] = int(self.map['total'] / 20)
+                self.map['mud'] = int(self.map['total'] / 20)
+            else:  # default, could also just omit condition or 'if True'
+                print("Must be 1-7")                                     # Ensures proper input
 
-    def print_settings(self, enum):
-        print("Creating ",                                           # Prints map size and style
-              enum.size(self.input_size).name, " ",
-              enum.style(self.input_style).name, "...")
-        print(self.map['trees'], " trees, ",                     # Prints quantity of each tile user can expect to find
-              self.map['boulder'], " boulders, ",
-              self.map['water'], " sqrft of water, and ",
-              self.map['mud'], " sqrft of mud.")
-        print("Creating player with ",                               # Prints user starting energy and money
-              self.player['energy'], " starting energy and ",
-              self.player['money'], " starting Ethereum.")
+    def player_stats(self):
+        self.player['energy'] = int(input("Input your player's starting energy(1-100): "))
+        while self.player['energy'] < 1 or self.player['energy'] > 100:
+            print("Must be 1-100")
+            self.player['energy'] = int(input("Input your player's starting energy(1-100): "))
+        self.player['money'] = int(input("Input your player's starting money(1-100): "))
+        while self.player['money'] < 1 or self.player['money'] > 100:
+            print("Must be 1-100")
+            self.player['money'] = int(input("Input your player's starting money(1-100): "))
+
+    def print_settings(self):
+        import enum
+
+        class Size(enum.Enum):  # Creates enum classes for print_settings
+            Small = 1
+            Medium = 2
+            Large = 3
+
+        class Style(enum.Enum):
+            Park = 1
+            Forest = 2
+            Rocky_Forest = 3
+            Rain_Forest = 4
+            Marsh = 5
+            Rocky_Swamp = 6
+            Quarry = 7
+        print()
+        print("Creating a",                                           # Prints map size and style
+              Size(self.input_size).name,
+              Style(self.input_style).name, ":")
+        print(self.map['trees'], "trees,",                     # Prints quantity of each tile user can expect to find
+              self.map['boulder'], "boulders,",
+              self.map['water'], "sqrft of water, and",
+              self.map['mud'], "sqrft of mud....")
+        print("Creating your player:")  # Prints user starting energy and money
+        print(self.player['energy'], "starting energy and",
+              self.player['money'], "starting Ethereum.")
+        print("Good Luck!")
+
+
+config = Config()
+config.print_settings()

b0db71a7d725e6dd110c8e4c2b0672ca7bd93875
Matthew Cole <macole@augusta.cs.pdx.edu>
2020-03-07 09:02:35 -0800

    added input checks in config

--- src/frupal/config.py
+++ src/frupal/config.py
@@ -18,8 +18,7 @@ class Config:
         self.map = {  # Creates Map Library
             'total': 100,  # Initializes map dimensions
             'height': 10,  # Initializes map height
-            'width': 10,  # Initializes map width
-            'jewel_var': 200
+            'width': 10  # Initializes map width
         }
 
         # Initialize Base Tiles with energy, counts, icons, color, and tools and their names and prices
@@ -385,7 +384,6 @@ class Config:
             else:  # default, for bad input
                 print("Must be 1-8")
                 self.map_Input['style'] = 0
-        self.map['jewel_var'] = int(input("(Jewel Gen) Please enter a number for X in 1/X: "))
 
     def change_stats(self, energy, money):  # Function sets player stats
         self.player['energy'] = energy
@@ -417,8 +415,7 @@ class Config:
                  "(Press S) Map Type: " + Size(self.map_Input['size']).name + " " + Type(self.map_Input['style']).name,
                  "(Press S) Map Size: " + '(' + str(self.map['height']) + "x" + str(
                      self.map['width']) + ' SQ. Yards) or '
-                 + str(self.map['total']) + " Tiles", "(Press S) Jewels: 1 per " + str(self.map['jewel_var']) +
-                 " Tiles!"]
+                 + str(self.map['total']) + " Tiles"]
         for key in self.tiles:
             r_str.append(
                 "(Press T) " + str(key).replace('_', ' ').capitalize() + ": " + str(
